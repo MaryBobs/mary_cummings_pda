b28ddb82118a93e2a03b81ed953046bd0918afd2
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-02-13 20:41:43 +0000

    operator functions test passing

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -23,4 +23,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('17')
   })
 
+  it("should update the display with result of operator buttons", function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('7')
+  })
+
 });

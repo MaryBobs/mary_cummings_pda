2386e9ee7808a77b4ed97498b1dfcb9e52f9ee4d
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-02-14 07:05:30 +0000

    chain together multiple operators test passing

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -32,4 +32,16 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('7')
   })
 
+  it("it should chain together multiple operations", function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#operator_equals')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_equals')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('256')
+  })
+
 });

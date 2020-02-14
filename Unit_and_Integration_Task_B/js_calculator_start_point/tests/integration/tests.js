7a549e47ca13089301871d48ae21fdc209d363e5
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-02-14 07:28:23 +0000

    displays very large numbers test passing

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -68,4 +68,22 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('1.8')
   })
 
+  it("should display very large numbers", function(){
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('70106020500')
+  })
+
 });
